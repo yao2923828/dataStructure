63900f648f39eb5c4021496f1d52cfb1b6c3f19e
yaoq <Phhv0JCx7Nn8fLVN>
2019-03-30 14:38:50 +0800

    调整

--- src/main/java/sort/dataStructure/insertsort/ShellSort.java
+++ src/main/java/sort/dataStructure/insertsort/ShellSort.java
@@ -13,23 +13,22 @@ import sort.dataStructure.Sort;
  */
 public class ShellSort implements Sort {
 
-    public int [] sort(int a[]) {
+    public int[] sort(int a[]) {
         int i, j, gap;
         int n = a.length;
-        for (gap = n / 2; gap > 0; gap /= 2) //步长
-            for (i = 0; i < gap; i++)        //直接插入排序
-            {
-                for (j = i + gap; j < n; j += gap)
-                    if (a[j] < a[j - gap]) {
-                        int temp = a[j];
-                        int k = j - gap;
-                        while (k >= 0 && a[k] > temp) {
-                            a[k + gap] = a[k];
-                            k -= gap;
-                        }
-                        a[k + gap] = temp;
+        for (gap=n/2;gap>0;gap/=2){
+            for(i=0;i<gap;i++){
+                for(j=i;j<n;j+=gap){
+                    int temp=a[j];
+                    int k=j-gap;
+                    while(k>=0&&a[k]>temp){
+                        a[k+gap]=a[k];
+                        k-=gap;
                     }
+                    a[k+gap]=temp;
+                }
             }
+        }
         return a;
     }
 
--- src/main/java/sort/dataStructure/insertsort/SortArithMetic.java
+++ src/main/java/sort/dataStructure/insertsort/SortArithMetic.java
@@ -1,7 +1,6 @@
 package sort.dataStructure.insertsort;
 
 import sort.dataStructure.Sort;
-import sort.dataStructure.SortUtils;
 
 /**
  * <p>标题: </p>
@@ -16,13 +15,12 @@ public class SortArithMetic implements Sort {
     public int [] sort(int [] data){
         for(int i=0;i<data.length;i++){
             int temp=data[i];
-            int j=0;
-            //移动位置
-            for(j=i-1;j>=0;j--){
-                if(data[j]>temp){
-                    SortUtils.swap(data, j, j+1);
-                }
+            int j=i-1;
+            while (j>=0 && data[j]>temp){
+               data[j+1]=data[j--];
             }
+            data[j+1]=temp;
+
         }
         return data;
     }
