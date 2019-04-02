aa1d45081ed11f59900317e9ae600616ddb1531e
yaoq <Phhv0JCx7Nn8fLVN>
2019-04-02 11:17:01 +0800

    非线程

--- src/main/java/sort/dataStructure/insertsort/ShellSort.java
+++ src/main/java/sort/dataStructure/insertsort/ShellSort.java
@@ -9,6 +9,7 @@ import sort.dataStructure.Sort;
  * <p>创建时间: 2019/3/13 10:40</p> 
  * <p>作者：yaoq</p>
  * <p>修改历史记录：</p>
+ *
  * ====================================================================<br>
  */
 public class ShellSort implements Sort {
@@ -19,8 +20,8 @@ public class ShellSort implements Sort {
         for (gap=n/2;gap>0;gap/=2){
             for(i=0;i<gap;i++){
                 for(j=i;j<n;j+=gap){
-                    int temp=a[j];
-                    int k=j-gap;
+                    int temp=a[j];  //从有序区第一个元素开始
+                    int k=j-gap;  //从无序区最后一个元素开始
                     while(k>=0&&a[k]>temp){
                         a[k+gap]=a[k];
                         k-=gap;
