6a5e6a725d5fbf0a2ad7c3c004c52333866d90bd
yao2923828 <Phhv0JCx7Nn8fLVN>
2021-08-12 07:00:54 +0800

    动态规划

--- src/main/java/offer/动态规划.java
+++ src/main/java/offer/动态规划.java
@@ -1,5 +1,11 @@
 package offer;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 /**
  * <p>标题: </p>
  * <p>功能描述: </p>
@@ -10,4 +16,218 @@ package offer;
  * ====================================================================<br>
  */
 public class 动态规划 {
+    /**
+     * 斐波那契数列
+     */
+    public int Fibonacci(int n) {
+        if (n <= 1)
+            return n;
+        int[] fib = new int[n + 1];
+        fib[1] = 1;
+        for (int i = 2; i <= n; i++)
+            fib[i] = fib[i - 1] + fib[i - 2];
+        return fib[n];
+    }
+    public int Fibonacci2(int n) {
+        if (n <= 1)
+            return n;
+        int pre2 = 0, pre1 = 1;
+        int fib = 0;
+        for (int i = 2; i <= n; i++) {
+            fib = pre2 + pre1;
+            pre2 = pre1;
+            pre1 = fib;
+        }
+        return fib;
+    }
+
+    /**
+     * 矩形覆盖
+     */
+    public int rectCover(int n) {
+        if (n <= 2)
+            return n;
+        int pre2 = 1, pre1 = 2;
+        int result = 0;
+        for (int i = 3; i <= n; i++) {
+            result = pre2 + pre1;
+            pre2 = pre1;
+            pre1 = result;
+        }
+        return result;
+    }
+
+    /**
+     * 跳台阶
+     */
+    public int JumpFloor(int n) {
+        if (n <= 2)
+            return n;
+        int pre2 = 1, pre1 = 2;
+        int result = 0;
+        for (int i = 2; i < n; i++) {
+            result = pre2 + pre1;
+            pre2 = pre1;
+            pre1 = result;
+        }
+        return result;
+    }
+
+    /**
+     * 变态跳台阶
+     */
+    public int jumpFloorII(int target) {
+        int[] dp = new int[target];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < target; i++)
+            for (int j = 0; j < i; j++)
+                dp[i] += dp[j];
+        return dp[target - 1];
+    }
+
+    /**
+     * 连续子数组的最大和
+     */
+    public int FindGreatestSumOfSubArray(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int greatestSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int val : nums) {
+            sum = sum <= 0 ? val : sum + val;
+            greatestSum = Math.max(greatestSum, sum);
+        }
+        return greatestSum;
+    }
+
+    /**
+     * 礼物的最大价值
+     */
+    public int getMost(int[][] values) {
+        if (values == null || values.length == 0 || values[0].length == 0)
+            return 0;
+        int n = values[0].length;
+        int[] dp = new int[n];
+        for (int[] value : values) {
+            dp[0] += value[0];
+            for (int i = 1; i < n; i++)
+                dp[i] = Math.max(dp[i], dp[i - 1]) + value[i];
+        }
+        return dp[n - 1];
+    }
+
+    /**
+     * 最长不含重复字符的子字符串
+     */
+    public int longestSubStringWithoutDuplication(String str) {
+        int curLen = 0;
+        int maxLen = 0;
+        int[] preIndexs = new int[26];
+        Arrays.fill(preIndexs, -1);
+        for (int curI = 0; curI < str.length(); curI++) {
+            int c = str.charAt(curI) - 'a';
+            int preI = preIndexs[c];
+            if (preI == -1 || curI - preI > curLen) {
+                curLen++;
+            } else {
+                maxLen = Math.max(maxLen, curLen);
+                curLen = curI - preI;
+            }
+            preIndexs[c] = curI;
+        }
+        maxLen = Math.max(maxLen, curLen);
+        return maxLen;
+    }
+
+    /**
+     * 丑数
+     */
+    public int GetUglyNumber_Solution(int N) {
+        if (N <= 6)
+            return N;
+        int i2 = 0, i3 = 0, i5 = 0;
+        int[] dp = new int[N];
+        dp[0] = 1;
+        for (int i = 1; i < N; i++) {
+            int next2 = dp[i2] * 2, next3 = dp[i3] * 3, next5 = dp[i5] * 5;
+            dp[i] = Math.min(next2, Math.min(next3, next5));
+            if (dp[i] == next2)
+                i2++;
+            if (dp[i] == next3)
+                i3++;
+            if (dp[i] == next5)
+                i5++;
+        }
+        return dp[N - 1];
+    }
+
+    /**
+     * n 个骰子的点数
+     */
+    public List<Map.Entry<Integer, Double>> dicesSum(int n) {
+        final int face = 6;
+        final int pointNum = face * n;
+        long[][] dp = new long[n + 1][pointNum + 1];
+
+        for (int i = 1; i <= face; i++)
+            dp[1][i] = 1;
+
+        for (int i = 2; i <= n; i++)
+            for (int j = i; j <= pointNum; j++)     /* 使用 i 个骰子最小点数为 i */
+                for (int k = 1; k <= face && k <= j; k++)
+                    dp[i][j] += dp[i - 1][j - k];
+
+        final double totalNum = Math.pow(6, n);
+        List<Map.Entry<Integer, Double>> ret = new ArrayList<>();
+        for (int i = n; i <= pointNum; i++)
+            ret.add(new AbstractMap.SimpleEntry<>(i, dp[n][i] / totalNum));
+
+        return ret;
+    }
+
+    public List<Map.Entry<Integer, Double>> dicesSum2(int n) {
+        final int face = 6;
+        final int pointNum = face * n;
+        long[][] dp = new long[2][pointNum + 1];
+
+        for (int i = 1; i <= face; i++)
+            dp[0][i] = 1;
+
+        int flag = 1;                                     /* 旋转标记 */
+        for (int i = 2; i <= n; i++, flag = 1 - flag) {
+            for (int j = 0; j <= pointNum; j++)
+                dp[flag][j] = 0;                          /* 旋转数组清零 */
+
+            for (int j = i; j <= pointNum; j++)
+                for (int k = 1; k <= face && k <= j; k++)
+                    dp[flag][j] += dp[1 - flag][j - k];
+        }
+
+        final double totalNum = Math.pow(6, n);
+        List<Map.Entry<Integer, Double>> ret = new ArrayList<>();
+        for (int i = n; i <= pointNum; i++)
+            ret.add(new AbstractMap.SimpleEntry<>(i, dp[1 - flag][i] / totalNum));
+
+        return ret;
+    }
+
+    /**
+     * 构建乘积数组
+     * 思考过程：先循环所有元素，逐个与剩余的元素做乘积，明显时间复杂度就是O(n^2)，所以直接不考虑。
+     * 最简单的办法就是所有元素相乘，然后除以每个元素的值，得到剩余元素的乘积，但是题目不允许这样计算，故废弃。
+     */
+    public int[] multiply(int[] A) {
+        int n = A.length;
+        int[] B = new int[n];
+        for (int i = 0, product = 1; i < n; product *= A[i], i++)       /* 从左往右累乘 */
+            B[i] = product;
+        for (int i = n - 1, product = 1; i >= 0; product *= A[i], i--)  /* 从右往左累乘 */
+            B[i] *= product;
+        return B;
+    }
+    public static void main(String[] args) {
+        动态规划 solution=new 动态规划();
+
+        solution.multiply(new int[]{1,2,3,4,5});
+    }
 }
